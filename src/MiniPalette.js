2ee7517279d7e4243b83934b63b5ff62c9b12bc4
Ankit Podder <ankitpodder0211@gmail.com>
2020-10-29 14:33:32 +0530

    remove bug and add functionality to del btn

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,9 +2,17 @@ import React , {Component} from 'react';
 import { withStyles } from "@material-ui/styles";
 import { withRouter } from "react-router-dom";
 import style from "./stylesheet/MiniPalette";
+import DeleteIcon from "@material-ui/icons/Delete";
 import { v4 as uuid } from "uuid";
 
 class MiniPalette extends Component{
+    constructor(props) {
+        super(props);
+        this.delelePalette = this.delelePalette.bind(this);
+    }
+    delelePalette() {
+        this.props.delelePalette(this.props.paletteName);
+    }
     render() {
         const { classes , paletteName , emoji , colors , id} = this.props;
         return (
@@ -16,6 +24,14 @@ class MiniPalette extends Component{
                     {colors.map(color => 
                         <div key={uuid()} className={classes.smallColorContainer} style={{background : color.color}}></div>    
                     )}
+                </div>
+                <div
+                    className={classes.deleteContainer}
+                    onClick={e => e.stopPropagation()}
+                >
+                    <DeleteIcon
+                        onClick={this.delelePalette}
+                    />
                 </div>
                 <div className={classes.details}>
                     <h5>{paletteName}</h5>
