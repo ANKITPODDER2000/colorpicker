c641e5b2f260f4436667aff59de10b668d5fa9de
Ankit Podder <ankitpodder0211@gmail.com>
2020-10-30 16:20:06 +0530

    Updates

---
+++ precache-manifest.77025fed46a58fa81f7b64477644e9e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c523d03fcd7a258446489d0831433fa2",
+    "url": "/colorpicker/index.html"
+  },
+  {
+    "revision": "2692d8102658e05cea29",
+    "url": "/colorpicker/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "2915d94c7f0b079a30dc",
+    "url": "/colorpicker/static/css/main.5ea5646e.chunk.css"
+  },
+  {
+    "revision": "2692d8102658e05cea29",
+    "url": "/colorpicker/static/js/2.45cf7d75.chunk.js"
+  },
+  {
+    "revision": "89c83479ebcf6bafd86be133d3697409",
+    "url": "/colorpicker/static/js/2.45cf7d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2915d94c7f0b079a30dc",
+    "url": "/colorpicker/static/js/main.8aac4cee.chunk.js"
+  },
+  {
+    "revision": "3aca2fc1879197d6c2c8",
+    "url": "/colorpicker/static/js/runtime-main.61807fb4.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/colorpicker/static/media/bg.77428cd4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bec26b8210bafa154aa44c4b1282cd16.js"
+  "/colorpicker/precache-manifest.77025fed46a58fa81f7b64477644e9e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/colorpicker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
